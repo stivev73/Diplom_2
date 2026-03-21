fa1d265ab5bb98f0a74711a9f2c7eb8a341946ed
Ivan Stepanov <stepanov.ivan.e@yandex.ru>
2026-03-21 14:42:53 +0300

    2

---
+++ data/ingredients.py
@@ -0,0 +1,17 @@
+class Ingredients:
+    """Дата для создания заказов"""
+
+    # Корректная дата ингредиентов
+    correct_ingredients_data = {
+        "ingredients": ["61c0c5a71d1f82001bdaaa6d", "61c0c5a71d1f82001bdaaa6f"]
+        }
+
+    # Некорретная дата ингредиентов (хэш)
+    incorrect_ingredients_data_hash = {
+        "ingredients": ["60d3b41abdacsdf6a733c6", "609646e4dcsfdf0276b2870"]
+        }
+
+    # Некорретная дата ингредиентов (пустая)
+    incorrect_ingredients_data_without_filling = {
+        "ingredients": []
+        }
